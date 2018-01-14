b16558fc053f97e5d59b4ecd0f6e4c134523c32a
ThuyTrang144 <thuytrangnguyen565@gmail.com>
2018-01-14 19:25:08 +0700

    create student

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
+# Rails.application.routes.draw do
+#   resources :students
+#   resources :users
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html]
+#   # namespace :admin do
+#   root "users#index"
+#   # end
+# end
+
 Rails.application.routes.draw do
+  resources :students
+  # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html]
-  namespace :admin do
-  root "admin#index"
-  end
+  # namespace :admin do
+  root "students#index"
+  # end
 end
