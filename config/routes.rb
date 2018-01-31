04b27cd2ce6d5137c5bb20b9d477e76bc7e2e612
ThuyTrang144 <thuytrangnguyen565@gmail.com>
2018-01-31 21:40:30 +0700

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   resources :books 
   # only: [:index, :new, :create, :edit]
   root "books#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=======
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+>>>>>>> 29a56ba... title
 end
