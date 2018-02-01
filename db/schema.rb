41989c0d4c3130f70a46559279c7dec785014e1b
ThuyTrang144 <thuytrangnguyen565@gmail.com>
2018-02-01 20:29:05 +0700

    Login

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180114131203) do
+ActiveRecord::Schema.define(version: 20180201132535) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
